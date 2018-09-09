e24800fe380655a253e81ef9bb3273b92d1bf6a3
Roman Tyurin <tyurroma@yandex.ru>
2018-09-09 23:03:03 +0300

    Add task 2

--- lesson_2/task2_string_compare.py
+++ lesson_2/task2_string_compare.py
@@ -1,1 +1,16 @@
 def compare(str1, str2):
+    if str1.isdigit() == True and str2.isdigit() == True:
+        print(0)
+    if str1 == str2:
+        print(1)
+    else:
+        if len(str1) > len(str2):
+            print(2)
+        elif str2 == 'learn':
+            print(3)
+
+
+compare('str1', 'str1')
+compare('ghdbtnsadsdf', 'str23sd')
+compare('1', '2')
+compare('learn', 'learn')
