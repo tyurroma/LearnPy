e1f9f40c08043fd9da0c22ecd4f8208825463156
Roman Tyurin <tyurroma@yandex.ru>
2018-09-09 11:50:44 +0300

    Add task1

--- lesson_functions.py
+++ lesson_functions.py
@@ -1,3 +1,14 @@
+#task1
+print('Task 1')
+def get_summ(one, two, delimiter='&'):
+    return str(one.upper()) + str(delimiter) + str(two.upper())
+
+sum_string = get_summ('Learn', 'Python')
+print(sum_string)
+
+#task2
+print('')
+print('Task 2')
 def format_price(price):
     price = int(price)
     return 'Price is: {}'.format(price)
