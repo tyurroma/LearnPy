1f06b7203c99ba64f600c5784b00c005dda32c7e
Roman Tyurin <tyurroma@yandex.ru>
2018-09-09 11:07:14 +0300

    Initial commit, lists lesson

---
+++ lesson_lists.py
@@ -0,0 +1,16 @@
+#task1
+print('Task 1')
+L = [2, 3, 4, 5, 6, 7]
+print(L)
+L.append('Python')
+print(L)
+print(len(L))
+
+#task2
+print('Task 2')
+print(L[0])
+print(L[-1])
+print(L[1:5])
+
+L.remove('Python')
+print(L)
