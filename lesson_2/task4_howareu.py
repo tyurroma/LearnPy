b5f1010e8880038ed1bbb1e73b34e423d3e73294
Roman Tyurin <tyurroma@yandex.ru>
2018-09-10 08:49:56 +0300

    Solved task4

---
+++ lesson_2/task4_howareu.py
@@ -0,0 +1,22 @@
+'''
+Напишите функцию ask_user(), которая с помощью input() спрашивает пользователя
+“Как дела?”, пока он не ответит “Хорошо”. Создайте словарь типа "вопрос": "ответ",
+например: {"Как дела": "Хорошо!", "Что делаешь?": "Программирую"} и так далее. 
+Доработайте ask_user() так, чтобы когда пользователь вводил вопрос который есть в
+словаре, программа давала ему соотвествующий ответ.
+'''
+
+q_a = {'How are you?': 'Fine', 'What are you doing?': 'Developing'}
+
+def ask_user():
+    while True:
+        user_text = input('Enter something: ')
+        if user_text == 'Good':
+            print('Bye!')
+            break
+        else:
+            for count in q_a:
+                if count == user_text:
+                    print(q_a[count])
+
+ask_user()
