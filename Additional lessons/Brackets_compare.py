47c52204016882d46513f93f0f3110b0891e642f
Roman Tyurin <tyurroma@yandex.ru>
2018-09-14 23:33:10 +0300

    Final commit

--- Additional lessons/Brackets_compare.py
+++ Additional lessons/Brackets_compare.py
@@ -1,34 +1,24 @@
 def brackets_compare(str1):
     open_bracket = '('
     close_bracket = ')'
-    k1 = 0
-    k2 = 0
-    
+    counter = 0
+
     for i in str1:
         if i == open_bracket:
-            k1 += 1
-            if str1[-1] == open_bracket:
-                return False
-                continue
+            counter += 1
         if i == close_bracket:
-            k2 += 1
-            if str1[0] == close_bracket:
-                return False
-                continue
-    if k1 == k2:
+            counter -= 1
+            if counter < 0:
+                break
+
+    if counter == 0:
         return True
     else:
         return False
 
 if __name__ == '__main__':
-    str1 = ')()('
+    str1 = '())(()'
     print(brackets_compare(str1))
-    str1 = '())()('
-    print(brackets_compare(str1))
-    str1 = ')(()())'
-    print(brackets_compare(str1))
-    str1 = '((())()('
-    print(brackets_compare(str1))
-    str1 = '())('
+    str1 = '()()()'
     print(brackets_compare(str1))
 
